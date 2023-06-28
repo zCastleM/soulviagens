254e549ea0a5fccd1bae958b415bd874e1eba38b
Mikael Meira <mikaelmenardi@gmail.com>
2023-06-28 11:35:18 -0300

    .env

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3pX3W6AXBLDZzUlDwEG3e3iobCY3hdtU",
-  authDomain: "soulviagens-cda0e.firebaseapp.com",
-  projectId: "soulviagens-cda0e",
-  storageBucket: "soulviagens-cda0e.appspot.com",
-  messagingSenderId: "702831888237",
-  appId: "1:702831888237:web:66f6ada1b2ad59505ebc64"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
